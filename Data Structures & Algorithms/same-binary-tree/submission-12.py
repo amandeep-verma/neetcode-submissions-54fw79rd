faf8343a249808dbe7e7adcb2501b41af1d15120
amandeep-verma <39786879+amandeep-verma@users.noreply.github.com>
2026-09-06 02:51:00 -0400

    Add: same-binary-tree - submission-12

---
+++ Data Structures & Algorithms/same-binary-tree/submission-12.py
@@ -0,0 +1,51 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+
+        stackOne = []
+        stackTwo = []
+
+        if p:
+            stackOne.append(p)
+        if q:
+            stackTwo.append(q)
+        
+
+        while stackOne and stackTwo:
+            currO = stackOne.pop()
+            currT = stackTwo.pop()
+
+            if currO.val != currT.val:
+                return False
+
+            # if currO.left:
+            #     stackOne.append(currO.left)
+            #     if not currT.left:
+            #         return False
+            # if currT.left:
+            #     stackTwo.append(currT.left)
+
+            if currO.left and currT.left:
+                stackOne.append(currO.left)
+                stackTwo.append(currT.left)
+            elif currO.left or currT.left:
+                return False
+
+            
+            
+            if currO.right:
+                stackOne.append(currO.right)
+                if not currT.right:
+                    return False
+            if currT.right:  
+                stackTwo.append(currT.right)
+            
+
+        return (not stackOne and not stackTwo)
+
