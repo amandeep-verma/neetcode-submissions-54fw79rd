6664a0fec7598c5eff52b434ddd3a0955ea2db38
amandeep-verma <39786879+amandeep-verma@users.noreply.github.com>
2026-09-08 03:18:51 -0400

    Add: lowest-common-ancestor-in-binary-search-tree - submission-8

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-8.py
@@ -0,0 +1,49 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+
+    def dfs(self, node: TreeNode, p: TreeNode, q: TreeNode, l, r, v) -> TreeNode:
+
+        if l and r:
+            return False, False, False
+
+        if not node:
+            return False, False, False
+
+        v1, l1 ,r1 = self.dfs(node.left, p, q, l, r, v)
+        v2, l2 ,r2 = self.dfs(node.right, p, q, l, r, v)
+
+        l = l1 or l2
+        r = r1 or r2
+
+        if node.val == p.val:
+            l = True
+        elif node.val == q.val:
+            r = True
+        
+        if v1:
+            v = v1
+        elif v2:
+            v = v2
+
+        if not v and l and r:
+            v = node
+
+        return v, l , r
+
+        
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+
+        v, l , r = self.dfs(root,p,q, False, False, False)
+
+        return v
+
+
+
+
+        
