b365264f9995d0a2a35e55f00e26e1f7b682cacd
amandeep-verma <39786879+amandeep-verma@users.noreply.github.com>
2026-09-14 17:30:03 -0400

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-15

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-15.py
@@ -0,0 +1,56 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+
+
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+
+
+        """
+        preorder - nlr
+        inorder - lnr
+        
+        Sol 1 - Recursive.
+        O(n*n)
+        """
+
+        # node = None
+        # if preorder:
+        #     node = TreeNode(preorder[0])
+        #     indexI = inorder.index(node.val)
+        #     node.left = self.buildTree(preorder[1:indexI+1], inorder[:indexI])
+        #     node.right = self.buildTree(preorder[indexI+1:], inorder[indexI+1:])
+
+        # return node
+
+        """
+        Sol 2 - using map and iterative ( saving indexing )
+        O(n*n)
+        """
+        myMap = {val: i for i, val in enumerate(inorder)}
+        node = None
+        
+        def dfs(l, r, li, ri):
+
+            node = None
+            if l<=r and li<=ri:
+                
+                node = TreeNode(preorder[l])
+                indexI = myMap.get(node.val)
+                leftSize = indexI - li
+
+                node.left = dfs(l + 1, l + leftSize, li, indexI - 1)
+
+                node.right = dfs(l + leftSize + 1, r, indexI + 1, ri )
+
+                # node.left = dfs(l+1 , indexI, li ,indexI -1)
+                # node.right = dfs(indexI+1, r, indexI+1 ,ri)
+            return node
+
+        return dfs(0, len(inorder)-1, 0, len(inorder)-1)
+
